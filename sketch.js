6420370e404e978e103cff573a9eba680c3fc416
Dheer542 <karuhars@gmail.com>
2020-08-24 10:59:42 +0530

    created the plinko game

--- sketch.js
+++ sketch.js
@@ -1,9 +1,15 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var engine, world;
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(480,800);
+ platform1 = new Ground(240,790,480,20);
 }
 
 function draw() {
-  background(255,255,255);  
+  background("black");  
+  platform1.display();
   drawSprites();
 }
